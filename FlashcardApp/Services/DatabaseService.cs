32cf462a3bb20844c35ee7e237cb4f909a7ba2c4
Ahmet Yusuf Söylemezoğlu <ahmetys2005@gmail.com>
2026-01-06 21:26:44 +0300

    database is working now. It just needs append operation to be working as well.

--- FlashcardApp/Services/DatabaseService.cs
+++ FlashcardApp/Services/DatabaseService.cs
@@ -1,5 +1,4 @@
 ﻿using SQLite;
-using FlashcardApp.CardDecks; 
 
 namespace FlashcardApp.Services
 {
@@ -12,23 +11,30 @@ namespace FlashcardApp.Services
             if (_database is not null)
                 return; 
             
-            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "flashcards.db");
-
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "Flashcards.db3");
+            
             _database = new SQLiteAsyncConnection(dbPath);
             
-            await _database.CreateTableAsync<decksData>();
+            await _database.CreateTableAsync<Flashcard>();
         }
 
-        public async Task AddCard(decksData yeniKart)
-        {
-            await Init(); 
-            await _database.InsertAsync(yeniKart);
-        }
-
-        public async Task<List<decksData>> GetAllCards()
+        public async Task<List<Flashcard>> GetFlashcardsAsync()
         {
             await Init();
-            return await _database.Table<decksData>().ToListAsync();
+            return await _database.Table<Flashcard>().ToListAsync();
+        }
+
+        public async Task<int> SaveFlashcardAsync(Flashcard card)
+        {
+            await Init();
+            if (card.ID != 0)
+            {
+                return await _database.UpdateAsync(card); // öncdeden ID varsa güncelle. 
+            }
+            else
+            {
+                return await _database.InsertAsync(card); 
+            }
         }
     }
 }
