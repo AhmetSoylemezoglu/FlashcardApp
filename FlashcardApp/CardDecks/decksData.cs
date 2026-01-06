495030f3df7f514dfc0efbf72ee4f0349f9cab8c
Ahmet Yusuf Söylemezoğlu <ahmetys2005@gmail.com>
2026-01-06 12:23:37 +0300

    basic sql table is done. Table "flashcards" have 3 value: ID PK, Front, Back.

---
+++ FlashcardApp/CardDecks/decksData.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace FlashcardApp.CardDecks
+{
+    [Table("flashcards")]
+    public class decksData
+    {
+        [PrimaryKey, AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("front_text")]
+        public string Front { get; set; }
+
+        [Column("back_text")]
+        public string Back { get; set; }
+    }
+}
