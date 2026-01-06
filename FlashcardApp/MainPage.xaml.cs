f0bafdaad0f3696f6736b631643b69b5ed03455b
Ahmet Yusuf Söylemezoğlu <ahmetys2005@gmail.com>
2026-01-06 20:38:39 +0300

    basic functions are over.

--- FlashcardApp/MainPage.xaml.cs
+++ FlashcardApp/MainPage.xaml.cs
@@ -9,17 +9,53 @@ public partial class MainPage : ContentPage
     int counter = 0;
     int max_card = 20;
     
-    private DatabaseService _veriServis;
+    private List<Flashcard> _flashcards = new List<Flashcard>();
+    private bool isShowingFront = true;
+    
+    //private DatabaseService _veriServis; database kullanmamaya karar verdim projenin yetişmesi için. 
     
     public MainPage(DatabaseService service)
     {
         InitializeComponent();
-        _veriServis = service;
+        LoadData();
+        max_card = _flashcards.Count-1;
+        CounterLabel.Text = (counter+1) + "/" + (max_card+1);
+        UpdateCard();
+        
+
+        //_veriServis = service;
+    }
+
+    private void LoadData()
+    {
+        _flashcards.Add(new Flashcard { Front = "Hola", Back = "Merhaba" });
+        _flashcards.Add(new Flashcard { Front = "Gato", Back = "Kedi" });
+        _flashcards.Add(new Flashcard { Front = "Perro", Back = "Köpek" });
+        _flashcards.Add(new Flashcard { Front = "Libro", Back = "Kitap" });
+        _flashcards.Add(new Flashcard { Front = "Programador", Back = "Yazılımcı" });
+    }
+
+    private void UpdateCard()
+    {
+        if (_flashcards.Count == 0) return; //Eğer liste boşsa direk döndür bir şey yapma demek bu.
+
+        Flashcard currentCard = _flashcards[counter];
+        
+        if (isShowingFront)
+        {
+            CardButton.Text = currentCard.Front;
+        }
+        else
+        {
+            CardButton.Text = currentCard.Back;
+        }
     }
 
     private void OnCardClicked(object? sender, EventArgs e)
     {
-        CardButton.Text = "basıldı";
+        isShowingFront = !isShowingFront;
+        UpdateCard();
+        //CardButton.Text = "basıldı";
     }
 
     private void OnPrevClicked(object? sender, EventArgs e)
@@ -28,7 +64,9 @@ public partial class MainPage : ContentPage
         {
             counter--;
         }
-        CounterLabel.Text = counter.ToString() + "/" + max_card;
+        CounterLabel.Text = (counter+1) + "/" + (max_card+1);
+        isShowingFront = true;
+        UpdateCard();
     }
     
     private void OnNextClicked(object? sender, EventArgs e)
@@ -37,7 +75,9 @@ public partial class MainPage : ContentPage
         {
             counter++;    
         }
-        CounterLabel.Text = counter.ToString() + "/" + max_card;
+        CounterLabel.Text = (counter+1) + "/" + (max_card+1);
+        isShowingFront = true;
+        UpdateCard();
     }
     
     private void OnFalseClicked(object? sender, EventArgs e)
