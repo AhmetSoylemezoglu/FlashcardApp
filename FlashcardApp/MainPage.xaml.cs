3057cba4118bdcbeae2da3ca3f2a8de50e3beac4
Ahmet Yusuf Söylemezoğlu <ahmetys2005@gmail.com>
2026-01-06 19:32:27 +0300

    basic button functionalities are added: next button, prev button, correct button, false button and card button.

--- FlashcardApp/MainPage.xaml.cs
+++ FlashcardApp/MainPage.xaml.cs
@@ -4,8 +4,11 @@ namespace FlashcardApp;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
+    int false_count = 0;
+    int correct_count = 0;
+    int counter = 0;
+    int max_card = 20;
+    
     private DatabaseService _veriServis;
     
     public MainPage(DatabaseService service)
@@ -14,8 +17,40 @@ public partial class MainPage : ContentPage
         _veriServis = service;
     }
 
-    private void OnCounterClicked(object? sender, EventArgs e)
+    private void OnCardClicked(object? sender, EventArgs e)
     {
-            
+        CardButton.Text = "basıldı";
     }
+
+    private void OnPrevClicked(object? sender, EventArgs e)
+    {
+        if (counter != 0)
+        {
+            counter--;
+        }
+        CounterLabel.Text = counter.ToString() + "/" + max_card;
+    }
+    
+    private void OnNextClicked(object? sender, EventArgs e)
+    {
+        if (counter != max_card)
+        {
+            counter++;    
+        }
+        CounterLabel.Text = counter.ToString() + "/" + max_card;
+    }
+    
+    private void OnFalseClicked(object? sender, EventArgs e)
+    {
+        false_count++;
+        FalseLabel.Text = "Yanlış: " + false_count;
+    }
+    
+    private void OnCorrectClicked(object? sender, EventArgs e)
+    {
+        correct_count++;
+        CorrectLabel.Text = "Doğru: " + correct_count;
+    }
+
+    
 }
